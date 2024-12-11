f60e39e4b3252791b77223955c4f4c87a73d8d45
SunShineCoco12 <134728775+SunShineCoco12@users.noreply.github.com>
2024-12-11 12:58:52 +0100

    Added delay to proximity fuze

--- torpedo.gd
+++ torpedo.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+@onready var fuzeDelay = $Timer
+
+var target = null
+var distance = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -16,11 +20,16 @@ func _on_area_2d_body_entered(body):
 		queue_free()
 
 func _on_detection_radii_body_entered(body):
+	target = body
 	if body.is_class("CharacterBody2D") and body.get_parent() != self.get_parent():
 		var relativePos = to_local(body.global_position)
-		var distance = sqrt(relativePos.x * relativePos.x + relativePos.y * relativePos.y)
-		body.HP -= 2500 / distance
-		print(relativePos)
+		distance = sqrt(relativePos.x * relativePos.x + relativePos.y * relativePos.y)
+		fuzeDelay.start()
+
+
+func _on_timer_timeout():
+	if target != null:
+		target.HP -= 2500 / distance
 		print(distance)
-		print(body.HP)
+		print(target.HP)
 		queue_free()
