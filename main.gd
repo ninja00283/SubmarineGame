2ca590481fa5a031e643468691e8518abe60345f
SunShineCoco12 <matthiasvanmeijeren2010@gmail.com>
2024-12-12 17:30:20 +0100

    Multiplayer, edited variables.
    Local multiplayer support alongside removing torpedoes' drag, and a start on adding other weaponry.

--- main.gd
+++ main.gd
@@ -1,37 +1,38 @@
 extends Node2D
 
-@onready var commandInput: LineEdit = $commandInput
-@onready var characterBody2D: CharacterBody2D = $CharacterBody2D
+@onready var commandInputP1: LineEdit = $Control/commandInputP1
+@onready var commandInputP2: LineEdit = $Control/commandInputP2
+@onready var characterBody2DP1: CharacterBody2D = $CharacterBody2DP1
+@onready var characterBody2DP2: CharacterBody2D = $CharacterBody2DP2
 @onready var torpedoScene = preload("res://torpedo.tscn")
 
 var commands = ["move", "fire"]
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	commandInput.grab_focus()
+var ammo = ["torpedo"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Submit"):
-		commandInterpret()
+		commandInterpret(commandInputP1, characterBody2DP1)
+	if Input.is_action_just_pressed("Submit"):
+		commandInterpret(commandInputP2, characterBody2DP2)
 
-func commandInterpret():
-	var text = commandInput.text.to_lower()
+func commandInterpret(input: LineEdit, characterBody: CharacterBody2D):
+	var text = input.text.to_lower()
 	var parts = text.split(" ")
 	if parts.size() > 0:
 		var command = parts[0]
 		if command in commands:
 			match command:
 				"move":
-					moveCommand(parts)
+					moveCommand(parts, characterBody)
 				"fire":
-					fireCommand(parts)
-			commandInput.clear()
+					fireCommand(parts, characterBody)
+			input.clear()
 			return
 	print("Unknown command: ", text)
-	commandInput.clear()
+	input.clear()
 
-func moveCommand(parts: Array):
+func moveCommand(parts: Array, characterBody: CharacterBody2D):
 	if parts.size() == 3:
 		var angleDegreesInput = 0
 		var magnitudeInput = 0
@@ -45,33 +46,37 @@ func moveCommand(parts: Array):
 			var x = magnitudeInput * cos(angleRadians)
 			var y = magnitudeInput * sin(angleRadians)
 			
-			characterBody2D.velocity += Vector2(x, y)
+			characterBody.velocity += Vector2(x, y)
 			print(x, " ", y, " Velocity added")
 		else:
 			print("Invalid move command. Both angle and magnitude must be numeric values.")
 	else:
 		print("Invalid move command. Expected 2 values: angle and magnitude.")
 
-func fireCommand(parts: Array):
-	print("fireCommand()")
+func fireCommand(parts: Array, characterBody: CharacterBody2D):
+	if parts.size() >= 3:
+		print("fireCommand()")
+		
+		var torpedo = torpedoScene.instantiate()
+		
+		var angleDegreesInput = 0
+		var magnitudeInput = 256
+		var ammo = parts[1]
+		var angle = parts[2]
+		
+		if angle.is_valid_float():
+			angleDegreesInput = angle.to_int()
+		
+		torpedo.rotation = deg_to_rad(angleDegreesInput)
+		
+		var direction = Vector2(cos(torpedo.rotation), sin(torpedo.rotation))
+		var velocity = direction * magnitudeInput
+		
+		torpedo.linear_velocity = velocity
+		
+		var offset = direction * 100
+		torpedo.position = characterBody.position + offset
 	
-	var torpedo = torpedoScene.instantiate()
-	
-	var angleDegreesInput = 0
-	var magnitudeInput = 256
-	var angle = parts[1]
-	
-	if angle.is_valid_float():
-		angleDegreesInput = angle.to_int()
-	
-	torpedo.rotation = deg_to_rad(angleDegreesInput)
-	
-	var direction = Vector2(cos(torpedo.rotation), sin(torpedo.rotation))
-	var velocity = direction * magnitudeInput
-	
-	torpedo.linear_velocity = velocity
-	
-	var offset = direction * 100
-	torpedo.position = characterBody2D.position + offset
-	
-	get_tree().root.add_child(torpedo)
+		get_tree().root.add_child(torpedo)
+	else:
+		print("Needs 3 parts, parts: ", parts.size())
